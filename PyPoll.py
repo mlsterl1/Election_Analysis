3faf5b6dcd9e5b3b39a79fe10bef5fb18b19ab73
lauren <mls_terl@yahoo.com>
2020-06-20 12:05:09 -0500

    PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -6,11 +6,20 @@
 # 5. The winner of the election based on popular vote. 
 # Assign a variable for the file to load and the path
 
-file_to_load = 'Election Results.csv'
+# Add our dependencies
+import csv
+import os
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join("Resources/Election Results.csv")
+   
+# Create a filename variable to save the file path
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
 # Open the election results and read the file. 
-election_data = open(file_to_load, 'r')
-# to do : perform analysis 
-# close the file. 
-election_data.close()
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
 
-
+    # Read and Print the header row. 
+    headers = next(file_reader)
+    print(headers)
+    
